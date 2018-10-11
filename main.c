c01c8f13779bae9ff0a9b9fef453bf640703c2d0
Tim Gold <timgold@gmail.com>
2018-10-11 13:22:39 +0300

    added string arg

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "main.h"
 
 void print_help()
@@ -18,6 +19,7 @@ int main(int argc,char **argv)
     int h_flag=0;
     int unknown_option=0;
     int arg_c;
+    char *string_value=NULL;
 
     if (argc==1)
     {
@@ -26,16 +28,29 @@ int main(int argc,char **argv)
         return -1;
     }
 
-    while ((arg_c=getopt(argc,argv,"h")) != -1)
+    while ((arg_c=getopt(argc,argv,"hs:")) != -1)
     {
         switch(arg_c)
         {
         case 'h':
             h_flag=1;
             break;
+        case 's':
+            //String_value will have the -s value
+            string_value=optarg;
+            break;
+        case '?':
+            if (optopt=='s')
+                fprintf(stderr,"No arument at -%c\n",optopt);
+            else if (isprint(optopt))
+                fprintf(stderr,"Unknown option -%c\n",optopt);
+            else   
+                fprintf(stderr,"Unknow char %x\n",optopt);
+            return 1;
         default:
             unknown_option=1;
             break;
+        
         } 
     }
 
